3fae9aee941c4841668ce93d3463fc3b346fab1e
Pedro Garcia <drogpe@gmail.com>
2020-10-26 09:18:46 -0300

    Created frontend app

--- elo_lead_manager/settings.py
+++ elo_lead_manager/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'rest_framework',
     'backend.apps.BackendConfig',
+    'frontend.apps.FrontendConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
